a863d8e7b567c5b17de878019d75e68bb6cbe5d7
FrentonGod <113124622+FrentonGod@users.noreply.github.com>
2023-03-29 14:43:27 -0600

    App.jsx para agregar Input1 e Input2 en InputTest

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from "react";
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState({
+    input1:"",
+    input2:"",
+  });
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-    console.log(e.target.value);
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value,
+    });
   }
 
-  return (
-    <div style={{textAlign: "center"}}>
+return (
+  <div style={{textAlign: "center"}}>
     <h1>Input Test</h1>
     <hr />
-    <input 
+    <label htmlFor="in1">Input1</label>
+    <input
+    id="in1"
+    name="input1"
     onChange={(e) => handleInputChange(e)}
+    placeholder="Este es el input1"
     type="text" 
-    value={inputValue}
+    value={inputValue.input1}
     />
+        <br />
+    <label>
+      Input2
+      <input 
+      name="input2"
+      className="mt-2"
+      onChange={(e) => handleInputChange(e)}
+      placeholder="Este es el input2"
+      type="text" 
+      value={inputValue.input2}
+      />
+    </label>
     </div>
   )
 }
